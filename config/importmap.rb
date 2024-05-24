62cfa555409da601dff48407cfae546e2e3b8b88
Tat Dat <tatdat97dhbkhn@gmail.com>
2024-05-24 10:57:58 +0700

    fix: not load avatar when broadcast (#4)

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin_all_from 'app/javascript/channels', under: 'channels'
 pin 'lodash' # @4.17.21
 pin '@rails/ujs', to: '@rails--ujs.js' # @7.0.8
 pin 'sweetalert2' # @11.11.0
